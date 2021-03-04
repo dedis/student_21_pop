15450dc5b2350c59cd6ad849969e05e141a8f89c
unknown <stefan.eric@epfl.ch>
2021-03-04 09:58:59 +0100

    starting cast Votes

---
+++ be1-go/db/castVotes.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+
+)
+
+func sendVote(laoId string, electionId string, database string, vote string) error{
+	channelId := []byte("root/"+ laoId +"/"+ electionId)
+	electionChannel := GetChannel(channelId, database)
+	//Data should be in the channel, once the users are
+	// subscribed they can see the data written inside it by
+	//a LAO member ----> it's response
+	//need somehow send a message through this electionChannel to cast
+	//a given votes
+	/* TODO : create a message that will represent the vote for the
+	given question with write-in being disabled  */
+
+}
